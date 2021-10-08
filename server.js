2e283e7d124903e50feef39cb5181cdc855eec14
Ryan <rmurph1800@gmail.com>
2021-10-07 22:13:19 -0400

    fixed public folder

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
-app.use(express.static("public/zookeepr-public"));
+app.use(express.static("public"));
 
 // app.get('/api/animals', (req, res) => {
 //   let results = animals;
